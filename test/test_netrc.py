b11c9280fbaa84b0a8b10dd0b4da16e64bfb8937
MahuMatei <m.mahu@student.vu.nl>
2024-06-27 17:13:07 +0200

    Improved 2 of the tests

--- test/test_netrc.py
+++ test/test_netrc.py
@@ -6,21 +6,29 @@ import sys
 import unittest
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-from youtube_dl.extractor import (
-    gen_extractors,
-)
-
+from youtube_dl.extractor import gen_extractors
 
 class TestNetRc(unittest.TestCase):
     def test_netrc_present(self):
+        extractors_with_login = []
+        extractors_without_login = []
+        
         for ie in gen_extractors():
-            if not hasattr(ie, '_login'):
-                continue
-            self.assertTrue(
-                hasattr(ie, '_NETRC_MACHINE'),
-                'Extractor %s supports login, but is missing a _NETRC_MACHINE property' % ie.IE_NAME)
-
+            if hasattr(ie, '_login'):
+                extractors_with_login.append(ie)
+                self.assertTrue(
+                    hasattr(ie, '_NETRC_MACHINE'),
+                    'Extractor %s supports login, but is missing a _NETRC_MACHINE property' % ie.IE_NAME
+                )
+                self.assertTrue(
+                    isinstance(ie._NETRC_MACHINE, str) and ie._NETRC_MACHINE,
+                    'Extractor %s has an invalid _NETRC_MACHINE property' % ie.IE_NAME
+                )
+            else:
+                extractors_without_login.append(ie)
+        
+        print(f"Checked {len(extractors_with_login)} extractors with login support.")
+        print(f"Checked {len(extractors_without_login)} extractors without login support.")
 
 if __name__ == '__main__':
     unittest.main()
--- test/test_options.py
+++ test/test_options.py
@@ -12,15 +12,47 @@ from youtube_dl.options import _hide_login_info
 
 
 class TestOptions(unittest.TestCase):
-    def test_hide_login_info(self):
-        self.assertEqual(_hide_login_info(['-u', 'foo', '-p', 'bar']),
-                         ['-u', 'PRIVATE', '-p', 'PRIVATE'])
-        self.assertEqual(_hide_login_info(['-u']), ['-u'])
-        self.assertEqual(_hide_login_info(['-u', 'foo', '-u', 'bar']),
-                         ['-u', 'PRIVATE', '-u', 'PRIVATE'])
-        self.assertEqual(_hide_login_info(['--username=foo']),
-                         ['--username=PRIVATE'])
+    def test_hide_login_info_basic(self):
+        self.assertEqual(
+            _hide_login_info(['-u', 'foo', '-p', 'bar']),
+            ['-u', 'PRIVATE', '-p', 'PRIVATE']
+        )
 
+    def test_hide_login_info_one_random(self):
+        self.assertEqual(_hide_login_info(['-u']), ['-u'])
+
+    def test_hide_login_info_multiple_users(self):
+        self.assertEqual(
+            _hide_login_info(['-u', 'foo', '-u', 'bar']),
+            ['-u', 'PRIVATE', '-u', 'PRIVATE']
+        )
+
+    def test_hide_login_info_long(self):
+        self.assertEqual(
+            _hide_login_info(['--username=foo']),
+            ['--username=PRIVATE']
+        )
+
+    def test_hide_login_info_password(self):
+        self.assertEqual(
+            _hide_login_info(['--password=bar']),
+            ['--password=PRIVATE']
+        )
+
+    def test_hide_login_info_combined(self):
+        self.assertEqual(
+            _hide_login_info(['-u', 'foo', '--password=bar']),
+            ['-u', 'PRIVATE', '--password=PRIVATE']
+        )
+
+    def test_hide_login_info_irrelevant_things(self):
+        self.assertEqual(
+            _hide_login_info(['-v', '--help']),
+            ['-v', '--help']
+        )
+
+    def test_hide_login_info_empty(self):
+        self.assertEqual(_hide_login_info([]), [])
 
 if __name__ == '__main__':
     unittest.main()
